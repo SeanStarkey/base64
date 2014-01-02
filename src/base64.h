8c9746f071d651052cf46860489e9602864e765f
Sean Starkey <sean@seanstarkey.com>
2014-01-02 09:37:49 -0700

    Add C++ to header

--- src/base64.h
+++ src/base64.h
@@ -24,6 +24,10 @@
 #ifndef _BASE64_H
 #define _BASE64_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /*
  * Note: The 'output' variable should be a preallocated buffer that
  * should have enough allocated space for the encoded/decoded
@@ -33,5 +37,9 @@
 void base64_encode(unsigned char *input, unsigned input_length, unsigned char *output);
 
 void base64_decode(unsigned char *input, unsigned input_length, unsigned char *output);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
